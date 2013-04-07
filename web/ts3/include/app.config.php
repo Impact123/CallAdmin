5e06ba6e2fdb0f2df349484019650eff24e04cd7
Impact <support@gugy.eu>
2013-04-07 23:56:09 +0200

    Added initial ts3 webscripts

---
+++ web/ts3/include/app.config.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ *---------------------------------------------------------------
+ * ACCESS SETTINGS
+ *---------------------------------------------------------------
+*/
+
+// This key must be sent in order to send reports
+$access_key = 'apitest';
+
+
+
+
+/*
+ *---------------------------------------------------------------
+ * CONNECTION SETTINGS
+ *---------------------------------------------------------------
+*/
+
+// Hostname of the ts3 server
+$host = "";
+
+// Serveradmin user
+$user = "serveradmin";
+
+
+//  Serveradmin password
+$password = "";
+
+
+// Server port (default is 9987)
+$port = 9987;
+
+
+// Server query port (default is 10011)
+$queryport = 10011;
+
+
+// UIDS of admins who receive reports
+$admins = Array('uid1', 'uid2', 'uid3');
+
+// End of file
---
+++ web/ts3/index.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * -----------------------------------------------------
+ * File        ts3call.php
+ * Authors     Impact, David <popoklopsi> Ordnung
+ * License     GPLv3
+ * Web         http://gugyclan.eu, http://popoklopsi.de
+ * -----------------------------------------------------
+ * 
+ * CallAdmin
+ * Copyright (C) 2013 Impact, David <popoklopsi> Ordnung
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>
+ */
+
+header("Content-type: text/xml; charset=utf-8"); 
+
+
+// Errors destroy the xmlvalidity
+//error_reporting(0);
+
+
+require_once("include/app.config.php");
+require_once("../include/calladmin_helpers.php");
+require_once("include/TeamSpeak3/TeamSpeak3.php");
+
+
+$helpers = new CallAdmin_Helpers();
+
+
+// Key set and no key given or key is wrong
+if( (!empty($access_key) && !isset($_GET['key']) ) || $_GET['key'] !== $access_key)
+{
+	$helpers->printXmlError("APP_AUTH_FAILURE", "CallAdmin_Ts3");
+}
+
+
+if(!isset($_GET['targetid']) 
+		|| !isset($_GET['targetname'])
+		|| !isset($_GET['targetreason'])
+		|| !isset($_GET['clientid'])
+		|| !isset($_GET['clientname'])
+		|| !isset($_GET['servername'])
+		|| !isset($_GET['serverip']))
+{
+	$helpers->printXmlError("APP_INPUT_FAILURE", "CallAdmin_Ts3");
+}
+
+
+//Variables
+$targetID     = $_GET['targetid'];
+$targetName   = $_GET['targetname'];
+$targetReason = $_GET['targetreason'];
+$clientID     = $_GET['clientid'];
+$clientName   = $_GET['clientname'];
+$serverName   = $_GET['servername'];
+$serverIP     = $_GET['serverip'];
+
+
+$targetCommBB = "Invalid";
+if($helpers->IsValidSteamID($targetID))
+{
+	$targetCommID = $helpers->SteamIDToComm($targetID);
+	$targetCommBB = "[url=http://steamcommunity.com/profiles/" . $targetCommID . "]$targetID" . "[/url]";
+}
+
+
+$clientCommBB = "Invalid";
+if($helpers->IsValidSteamID($clientID))
+{
+	$clientCommID = $helpers->SteamIDToComm($clientID);
+	$clientCommBB = "[url=http://steamcommunity.com/profiles/" . $clientCommID . "]$clientID" . "[/url]";
+}
+
+$connect = "[url=steam://connect/" . $serverIP . "]connect now[/url]";
+
+
+
+require_once("include/TeamSpeak3/TeamSpeak3.php");
+$ts3 = new TeamSpeak3();
+
+try
+{
+	$ts3_VirtualServer = TeamSpeak3::factory("serverquery://". $user . ":" . $password . "@" . $host . ":" . $queryport . "/?server_port=" . $port);
+	
+	$uid  = "";
+	//$name = "";
+	foreach($ts3_VirtualServer->clientList() as $ts3_Client)
+	{
+		$uid = (string)$ts3_Client['client_unique_identifier'];
+		//$name = (string)$ts3_Client['client_nickname'];
+		
+		
+		// Ss listed as admin, go send him a message
+		if(in_array($uid, $admins))
+		{
+			$ts3_Client->message("----------------------------------------------------");
+			$ts3_Client->message("[CallAdmin] New report on:   $serverName ($serverIP) $connect");
+			$ts3_Client->message("[CallAdmin] Reporter:        $targetName ($targetCommBB)");
+			$ts3_Client->message("[CallAdmin] Report reason:   $targetReason");
+			$ts3_Client->message("[CallAdmin] Reported player: $clientName ($clientCommBB)");
+			$ts3_Client->message("----------------------------------------------------");
+		}
+	}
+}
+catch(TeamSpeak3_Adapter_ServerQuery_Exception $e)
+{
+	// Nope
+	$helpers->printXmlError($e->getMessage(), "CallAdmin_Ts3");
+}
+
+$xml = new SimpleXMLElement("<CallAdmin_Ts3/>");
+$xml->addChild("success", "true");
+echo $xml->asXML();
+// End of file: ts3call.php
